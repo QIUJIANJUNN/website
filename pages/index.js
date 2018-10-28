5b600203aded02aba1e6d0edfb20189be555067f
Wayne Chiu <chaoweichiu@gmail.com>
2018-10-28 11:03:01 +0800

    add layout and its needed config

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
+import Layout from '../components/Layout'
 
 const ColorText = styled.div`
-  color: green;
+  color: white;
 `
 
 const IndexPage = () => (
-  <ColorText>
-    This is color
-  </ColorText>
+  <Layout>
+    <ColorText>
+      This is color
+    </ColorText>
+  </Layout>
 )
 
 export default IndexPage
