49accd94b124f3f3f3aeb6d11c0548e97b2f5680
Wayne Chiu <chaoweichiu@gmail.com>
2018-10-28 10:56:11 +0800

    add nextjs skeleton

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ColorText = styled.div`
+  color: green;
+`
+
+const IndexPage = () => (
+  <ColorText>
+    This is color
+  </ColorText>
+)
+
+export default IndexPage
